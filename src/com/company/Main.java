3a6328e327d8dd54a06ae3be83f2d33efb41a603
saleenadelgado <Saleena.delgado.2022@student.lhschools.org>
2020-02-20 12:03:53 -0500

    intellij commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("My Name Is ");
+        System.out.println("Saleena");
+
+        //print three more lines about what you did last summer
+        System.out.println("I Visited My Aunt");
+        System.out.println("I Hung out with friends");
+        System.out.println("I Got a puppy");
+
+    }
+}
